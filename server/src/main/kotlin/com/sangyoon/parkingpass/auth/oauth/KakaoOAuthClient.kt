80e046fb189fb85dc0e886ecb4f3a7319361a13c
Sangyoon <qlrqod123123@naver.com>
2026-01-08 00:50:24 +0900

    fix(user-mvp): 카카오 로그인 오류 수정

--- server/src/main/kotlin/com/sangyoon/parkingpass/auth/oauth/KakaoOAuthClient.kt
+++ server/src/main/kotlin/com/sangyoon/parkingpass/auth/oauth/KakaoOAuthClient.kt
@@ -4,7 +4,7 @@ import com.sangyoon.parkingpass.config.KakaoOAuthConfig
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.engine.cio.CIO
-.import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.accept
 import io.ktor.client.request.forms.FormDataContent
 import io.ktor.client.request.get
