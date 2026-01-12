6707f820b430e4b2f43519b627826f995dbce9e5
Sangyoon <qlrqod123123@naver.com>
2026-01-13 00:13:59 +0900

    feat(theme): App.kt에 ParkingPassTheme 다크 테마 적용

--- composeApp/src/commonMain/kotlin/com/sangyoon/parkingpass/App.kt
+++ composeApp/src/commonMain/kotlin/com/sangyoon/parkingpass/App.kt
@@ -1,12 +1,12 @@
 package com.sangyoon.parkingpass
 
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import com.sangyoon.parkingpass.di.appModule
 import com.sangyoon.parkingpass.presentation.navigation.ParkingAppNavigation
 import com.sangyoon.parkingpass.presentation.ui.LoginScreen
+import com.sangyoon.parkingpass.presentation.ui.theme.ParkingPassTheme
 import com.sangyoon.parkingpass.presentation.viewmodel.AuthViewModel
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.compose.KoinApplication
@@ -21,7 +21,7 @@ fun App() {
         val authViewModel = koinViewModel<AuthViewModel>()
         val authState by authViewModel.uiState.collectAsState()
 
-        MaterialTheme {
+        ParkingPassTheme {
             if (authState.currentUser == null) {
                 LoginScreen(viewModel = authViewModel)
             } else {
